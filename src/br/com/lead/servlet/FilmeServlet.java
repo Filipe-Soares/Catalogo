6d7a5f72e0ddf98ff76a894a2aaca2008b380ed1
filipe.soares <filipe.soares@host.docker.internal>
2021-01-13 08:46:21 -0300

    Update Pom

--- src/br/com/lead/servlet/FilmeServlet.java
+++ src/br/com/lead/servlet/FilmeServlet.java
@@ -1,9 +1,10 @@
 package br.com.lead.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,7 +18,7 @@ public class FilmeServlet extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Filme coringa = new Filme("Coringa", "Drama", 2019);
-		Filme matrix = new Filme("Matrix", "Ação", 1999);
+		Filme matrix = new Filme("Matrix", "Acao", 1999);
 		Filme forrestGump = new Filme("Forrest Gump", "Drama", 1994);
 		
 		ArrayList<Filme> filmes = new ArrayList<Filme>();
